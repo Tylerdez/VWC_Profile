fd06935c83e3bddcf02676ec953f64076ff4a365
Tyler <tyler@MacBook-Pro.local>
2020-10-08 19:52:17 -0400

    added contact form and changed background

--- script.js
+++ script.js
@@ -93,3 +93,92 @@ function myFunction() {
   var element = document.body;
   element.classList.toggle("dark-mode");
 }
+
+/************Weather API************************/
+
+// SELECT ELEMENTS
+const tempElement = document.querySelector(".temperature-value p");
+const descElement = document.querySelector(".temperature-description p");
+const locationElement = document.querySelector(".location p");
+
+// App data
+const weather = {};
+
+weather.temperature = {
+  unit: "fahrenheit",
+};
+
+// APP CONSTS AND VARS
+const KELVIN = 273;
+// API KEY
+const key = "";
+
+// CHECK IF BROWSER SUPPORTS GEOLOCATION
+if ("geolocation" in navigator) {
+  navigator.geolocation.getCurrentPosition(setPosition, showError);
+} else {
+  notificationElement.innerHTML = "<p>Browser doesn't Support Geolocation</p>";
+}
+
+// SET USER'S POSITION
+function setPosition(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+
+  getWeather(latitude, longitude);
+}
+
+// SHOW ERROR WHEN THERE IS AN ISSUE WITH GEOLOCATION SERVICE
+function showError(error) {
+  notificationElement.innerHTML = `<p> ${error.message} </p>`;
+}
+
+// GET WEATHER FROM API PROVIDER
+function getWeather(latitude, longitude) {
+  let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+//getting data
+  fetch(api)
+    .then(function (response) {  //promise //transforming data into something workable
+      let data = response.json();
+      return data;
+    })
+    .then(function (data) { //pop obj with data
+      weather.temperature.value = Math.floor(
+        1.8 * (data.main.temp - KELVIN) + 32 //took a while to get right
+      );
+      weather.description = data.weather[0].description.toUpperCase();
+      weather.city = data.name;
+      weather.country = data.sys.country;
+    })
+    .then(function () {
+      displayWeather();
+    });
+}
+
+// DISPLAY WEATHER TO UI
+function displayWeather() {
+  tempElement.innerHTML = `${weather.temperature.value}°F`;
+  descElement.innerHTML = weather.description;
+  locationElement.innerHTML = `${weather.city}, ${weather.country}`;
+}
+
+/**********End Weather API**********************/
+
+
+
+
+/***********Recreation Site API*****************/
+
+
+
+/*****************End Recreation API*****************/
+
+
+
+
+
+/***********Dive Site API*****************/
+
+
+
+/*****************End Dive Site API*****************/
